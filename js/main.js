b16deb60748bcf3de6c779caff3bab6fa3e0ff32
AlexItr2020 <63164986+AlexItr2020@users.noreply.github.com>
2021-06-28 21:52:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+$('.slider_inner, .news_slider_inner').slick(
+    {   infinite: false,
+        nextArrow:'<button type="button" class="slick_btn slick-next"></button>',
+        prevArrow:'<button type="button" class="slick_btn slick-prev"></button>'
+
+    }
+);
+
+
+
+})
+//$('select').styler();
+
